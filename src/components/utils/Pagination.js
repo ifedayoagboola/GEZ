7ba67d1b83c62b7d4f5449720a7f6e891fafa34a
manlikeghost <ekpoabasiama@gmail.com>
2022-02-02 17:29:37 +0100

    remove link comp

--- src/components/utils/Pagination.js
+++ src/components/utils/Pagination.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+
 
 const Pagination = ({ picturesPerPage, totalPictures, paginate }) => {
 	const pageNumbers = [];
