30a2fb9dd099d1e19beb4025f58da76810c51aad
ifedayo <ifedayoagboola19@gmail.com>
2022-02-03 03:59:21 +0100

    Update why-kaduna UI

--- src/components/Kaduna.js
+++ src/components/Kaduna.js
@@ -42,7 +42,7 @@ const Kaduna = () => {
           </div>
         </div>
       </section>
-      <div className="bg-gray-200 font-normal text-gray-700 text-base lg:text-base ">
+      <div className="bg-gray-200 font-normal text-gray-700 text-base lg:text-base">
         <p className="container mx-auto px-12 py-6">
           The Kaduna State Investment Promotion Agency (KADIPA) is a One-Stop
           Investment Centre for businesses and investments. As part of our
@@ -52,130 +52,162 @@ const Kaduna = () => {
           the state.
         </p>
       </div>
-      <section style={{ background: "#373A36" }}>
-        <div className="container mx-auto px-20 flex items-center">
-          <div>
-            <div className="flex justify-center items-center py-20">
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
 
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
+      <section>
+        <div className="text-base flex flex-col lg:flex-row h-full lg:h-screen w-full items-center lg:items-start ">
+          <div
+            className="w-full lg:w-1/2 h-screen"
+            style={{ background: "#EBEBEB", color: "#000" }}
+          >
+            <div className="container mx-auto px-8 lg:p-20 py-8">
+              <p className="text-2xl font-semibold text-gray-700">
+                WHY KADUNA
+                <span className="text-green-600 font-bold">/</span>
+              </p>
+              <br />
 
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
-            </div>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  INVESTOR-FRIENDLY ENVIRONMENT
+                </span>
+                Number one in ease of doing business (World Bank ranking).
+                Nigeria's leading state in transparency and open governance.
+                Easiest place to register land compared to other states.
+              </p>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  THE WONDERLAND OF AGRICULTURE
+                </span>
+                No. 1 producer of ginger. No. 1 producer of maize. Second
+                largest producer of soya beans.
+              </p>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  GATEWAY TO NORTHERN NIGERIA MARKETS
+                </span>
+                A natural bridge between 19 northern states, thus creating an
+                efficient and cost effective outlets to major markets providing
+                access to about 90 million customers in Nigeria.
+              </p>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  THIRD BIGGEST CONSUMER MARKET IN NIGERIA
+                </span>
+                Access to 52% of Nigeria's consumer market and third most
+                populous state in Nigeria.
+              </p>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  RICH IN MINERAL RESOURCES
+                </span>
+                Over 25 non-oil mineral deposits, including gold, iron ore,
+                marble. The state has over 25 mineral types across the entire 23
+                local governments.
+              </p>
 
-            <div className="flex py-20">
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
-
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
-
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
+              {/* <p className="cursor-pointer text-green-600 text-base font-semibold py-8">
+                LEARN MORE...
+              </p> */}
             </div>
           </div>
+          <div
+            className="w-full lg:w-1/2 h-screen bg-no-repeat bg-cover"
+            style={{
+              backgroundImage: "url(/assets/bg2.jpg)",
+            }}
+          ></div>
+        </div>
 
-          <div>
-            <div className="flex py-20">
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
+        <div className="text-base flex flex-col lg:flex-row h-full lg:h-screen w-full items-center lg:items-start ">
+          <div
+            className="w-full lg:w-1/2 h-screen bg-no-repeat bg-cover"
+            style={{
+              backgroundImage: "url(/assets/bg2.jpg)",
+            }}
+          ></div>
 
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
+          <div
+            className="w-full lg:w-1/2 h-screen"
+            style={{ background: "#EBEBEB", color: "#000" }}
+          >
+            <div className="container mx-auto px-8 lg:p-20 py-8">
+              <p className="font-light text-gray-700 py-6">
+                We have a reputation for providing streamlined, efficient, and
+                transparent services to our clients with a view to reducing the
+                cost of doing business in the state. Within the short period of
+                our existence, we have perfected mechanisms that shorten and
+                simplify administrative procedures for the issuance of business
+                approvals, permits and licenses, and company incorporation.
+              </p>
 
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
-            </div>
-            <div className="flex  py-20">
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  STEADY ECONOMIC GROWTH
+                </span>
+                Kaduna’s innovative governance initiatives have resulted in
+                steady economic growth. We have seen a 91% increase in IGR since
+                2016, leading to increases in the public service provision, with
+                an average of 8.82% YoY GBP growth.
+              </p>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  PRODUCTIVE WORKFORCE
+                </span>
+                Kaduna state has a large youthful population with a majority
+                within the productive age. We have further complemented this by
+                providing easy access to skilled manpower from the many quality
+                educational institution that support skills development.
+              </p>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  ABUNDANT NATUAL RESOURCES
+                </span>
+                In addition to its invaluable human resources Kaduna is blessed
+                with abundant natural resources to support agricultural and
+                industrial production.
+              </p>
+              <p
+                className="font-light text-gray-700"
+                style={{ borderRight: "2px solid green" }}
+              >
+                <span className="text-green-600 font-bold">
+                  FAVOURBALE BUSINESS REGULATORY ENVIRONMENT
+                </span>
+                Kaduna’s visionary leadership and innovative governance
+                initiatives have made the state one of the most convenient
+                places to do business in Nigeria.
+              </p>
 
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
-              <div className="w-1/2">
-                <p className="text-2xl font-semibold text-white">
-                  ZONE A<span className="text-green-600">/</span>
-                </p>
-                <img src={vision} alt="" />
-
-                <p className="text-white">
-                  <span className="text-5xl lg:text-6xl font-bold text-green-600">
-                    35
-                  </span>
-                  USD per sqm
-                </p>
-              </div>
+              {/* <p className="cursor-pointer text-green-600 text-base font-semibold py-8">
+                LEARN MORE...
+              </p> */}
             </div>
           </div>
         </div>
-      </section>
-      <section>
-        <WhyInvest />
       </section>
 
       <section className="">
