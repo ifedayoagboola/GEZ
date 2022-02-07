f9379b01234e9c15f512e5a77a84a422ddbb6b22
manlikeghost <ekpoabasiama@gmail.com>
2022-02-07 09:36:10 +0100

    animated and added a transiton effect

--- src/components/Clusters.js
+++ src/components/Clusters.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import StickyHeader from '../components/StickyHeader';
-import BusinessCluster from '../img/newsImages/Business-Cluster.png';
-import IndustrialCluster from '../img/newsImages/Industrial-Cluster.png';
 import map from '../img/gez-zones.jpeg';
 import NewsCard from './NewsCard';
 
@@ -13,7 +11,7 @@ const articles = [
 		date: 'June 6, 2022',
 		title: 'FOOD PROCESSING PARK',
 		content:
-			' Equipped with plug and play modern infrastructures enabling entrepreneurs to setup food processing units close to crop production areas with an integrated and complete preservation infrastructure and facilities.',
+			' Equipped with plug and play modern infrastructures enabling entrepreneurs to setup food processing units close to crop production areas with an integrated and complete preservation infrastructure and facilities. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut. Integer finibus arcu ac aliquet bibendum. Vivamus lacinia nibh nunc, ut fringilla diam porttitor vel. Etiam eget imperdiet orci. Proin justo.',
 		img: '../assets/art1.jpg',
 	},
 	{
@@ -21,7 +19,7 @@ const articles = [
 		date: 'June 22, 2022',
 		title: 'LIGHT INDUSTRY PARK',
 		content:
-			'A safe, secure and eco-friendly environment for firms in the light industry to tap into the huge potentials of providing food processing, paper making, plastic, leather, textiles, paper,  jute and cement manufacturing companies operating in the Industrial Park with services ranging from spare parts production to maintenance operations.',
+			'A safe, secure and eco-friendly environment for firms in the light industry to tap into the huge potentials of providing food processing, paper making, plastic, leather, textiles, paper,  jute and cement manufacturing companies operating in the Industrial Park with services ranging from spare parts production to maintenance operations. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut.',
 		img: '../assets/art2.jpg',
 	},
 	{
@@ -29,7 +27,7 @@ const articles = [
 		date: 'Just now',
 		title: 'MULTIMODAL LOGISTICS PARK',
 		content:
-			' Provides a cluster of logistics companies offering tailored, flexible and streamlined logistics processes, covering air, ocean, road and rail freight – with optimal cost-efficiency and flexibility in routing and transit times.',
+			' Provides a cluster of logistics companies offering tailored, flexible and streamlined logistics processes, covering air, ocean, road and rail freight – with optimal cost-efficiency and flexibility in routing and transit times. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut. Integer finibus arcu ac aliquet bibendum. Vivamus lacinia nibh nunc, ut fringilla diam porttitor vel. Etiam eget imperdiet orci.',
 		img: '../assets/art3.jpg',
 	},
 	{
@@ -37,7 +35,7 @@ const articles = [
 		date: 'January 22, 2022',
 		title: 'AGRO ALLIED PARK',
 		content:
-			' Ready to play host to Agro Allied Industries, the GEZ will facilitate the enabling environment for local and international firms to thrive and increase the contribution of the agriculture sector to GDP, foster wealth and employment creation for the youth.',
+			' Ready to play host to Agro Allied Industries, the GEZ will facilitate the enabling environment for local and international firms to thrive and increase the contribution of the agriculture sector to GDP, foster wealth and employment creation for the youth. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut. Integer finibus arcu ac aliquet bibendum.',
 		img: '../assets/art4.jpg',
 	},
 ];
@@ -55,13 +53,13 @@ const Clusters = () => {
 	return (
 		<div className=''>
 			<section className=''>
-				<div className='fixed w-full'>
+				<div className='fixed w-full z-40'>
 					{navbar ? <StickyHeader /> : <Header />}
 				</div>
 				<div
 					className='w-full h-screen bg-no-repeat bg-cover flex flex-col justify-center items-left'
 					style={{
-						backgroundImage: 'url(/assets/clusterLayout.jpg)',
+						backgroundImage: 'url(/assets/newsImages/cluster-gif.gif)',
 					}}
 				>
 					<div className='text-white text-left text-5xl lg:text-6xl font-bold container mx-auto px-8 lg:px-16 leading-tight mt-auto py-12'>
@@ -83,23 +81,8 @@ const Clusters = () => {
 			</div>
 			<section className='container mx-auto p-20'>
 				<div className='flex justify-evenly'>
-					<div>
-						<div className='my-8'>
-							<img src={BusinessCluster} alt='' width={550} height={550} />
-						</div>
-						<div className='my-8'>
-							<img src={IndustrialCluster} alt='' width={550} height={550} />
-						</div>
-					</div>
-					<div>
-						<img src={map} alt='' width={600} height={600} />
-					</div>
+					<img src={map} alt='' />
 				</div>
-				<a href='../../public/assets/aboutbg2.jpg' download>
-					<span className='cursor-pointer text-green-600 text-base font-semibold py-8'>
-						LEARN MORE...
-					</span>
-				</a>
 			</section>
 
 			<section className=''>
@@ -113,9 +96,9 @@ const Clusters = () => {
 							return <NewsCard key={article.id} article={article} />;
 						})}
 					</div>
-					<p className='cursor-pointer text-green-600 text-base font-semibold py-8'>
+					{/* <p className='cursor-pointer text-green-600 text-base font-semibold py-8'>
 						READ MORE NEWS...
-					</p>
+					</p> */}
 				</div>
 			</section>
 
