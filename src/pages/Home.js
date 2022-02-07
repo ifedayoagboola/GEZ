987aa2b7893ff33a703aa7a3282619fa05f813f8
manlikeghost <ekpoabasiama@gmail.com>
2022-02-07 09:44:33 +0100

    homepage article added more content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,39 +7,40 @@ import NewsCard from "../components/NewsCard";
 import StickyHeader from "../components/StickyHeader";
 
 const articles = [
-  {
-    id: "1",
-    date: "June 6, 2022",
-    title: "FOOD PROCESSING PARK",
-    content:
-      " Equipped with plug and play modern infrastructures enabling entrepreneurs to setup food processing units close to crop production areas with an integrated and complete preservation infrastructure and facilities.",
-    img: "../assets/art1.jpg",
-  },
-  {
-    id: "2",
-    date: "June 22, 2022",
-    title: "LIGHT INDUSTRY PARK",
-    content:
-      "A safe, secure and eco-friendly environment for firms in the light industry to tap into the huge potentials of providing food processing, paper making, plastic, leather, textiles, paper,  jute and cement manufacturing companies operating in the Industrial Park with services ranging from spare parts production to maintenance operations.",
-    img: "../assets/art2.jpg",
-  },
-  {
-    id: "3",
-    date: "Just now",
-    title: "MULTIMODAL LOGISTICS PARK",
-    content:
-      " Provides a cluster of logistics companies offering tailored, flexible and streamlined logistics processes, covering air, ocean, road and rail freight – with optimal cost-efficiency and flexibility in routing and transit times.",
-    img: "../assets/art3.jpg",
-  },
-  {
-    id: "4",
-    date: "January 22, 2022",
-    title: "AGRO ALLIED PARK",
-    content:
-      " Ready to play host to Agro Allied Industries, the GEZ will facilitate the enabling environment for local and international firms to thrive and increase the contribution of the agriculture sector to GDP, foster wealth and employment creation for the youth.",
-    img: "../assets/art4.jpg",
-  },
+	{
+		id: '1',
+		date: 'June 6, 2022',
+		title: 'FOOD PROCESSING PARK',
+		content:
+			' Equipped with plug and play modern infrastructures enabling entrepreneurs to setup food processing units close to crop production areas with an integrated and complete preservation infrastructure and facilities. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut. Integer finibus arcu ac aliquet bibendum. Vivamus lacinia nibh nunc, ut fringilla diam porttitor vel. Etiam eget imperdiet orci. Proin justo.',
+		img: '../assets/art1.jpg',
+	},
+	{
+		id: '2',
+		date: 'June 22, 2022',
+		title: 'LIGHT INDUSTRY PARK',
+		content:
+			'A safe, secure and eco-friendly environment for firms in the light industry to tap into the huge potentials of providing food processing, paper making, plastic, leather, textiles, paper,  jute and cement manufacturing companies operating in the Industrial Park with services ranging from spare parts production to maintenance operations. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut.',
+		img: '../assets/art2.jpg',
+	},
+	{
+		id: '3',
+		date: 'Just now',
+		title: 'MULTIMODAL LOGISTICS PARK',
+		content:
+			' Provides a cluster of logistics companies offering tailored, flexible and streamlined logistics processes, covering air, ocean, road and rail freight – with optimal cost-efficiency and flexibility in routing and transit times. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut. Integer finibus arcu ac aliquet bibendum. Vivamus lacinia nibh nunc, ut fringilla diam porttitor vel. Etiam eget imperdiet orci.',
+		img: '../assets/art3.jpg',
+	},
+	{
+		id: '4',
+		date: 'January 22, 2022',
+		title: 'AGRO ALLIED PARK',
+		content:
+			' Ready to play host to Agro Allied Industries, the GEZ will facilitate the enabling environment for local and international firms to thrive and increase the contribution of the agriculture sector to GDP, foster wealth and employment creation for the youth. Proin tristique mattis elementum. Nam nec gravida purus, sit amet varius quam. In porta molestie mi, eu aliquet odio tempus ut. Integer finibus arcu ac aliquet bibendum.',
+		img: '../assets/art4.jpg',
+	},
 ];
+
 const Home = () => {
   const [navbar, setNavbar] = useState(false);
   const navHandler = () => {
